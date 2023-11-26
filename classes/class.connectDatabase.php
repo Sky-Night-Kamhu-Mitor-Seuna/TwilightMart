ac9d41edc8635acfb947c078bd4a586442558edd
Mai.Coding <74230665+0x0607@users.noreply.github.com>
2023-11-26 21:40:00 +0800

    hi 5026

--- classes/class.connectDatabase.php
+++ classes/class.connectDatabase.php
@@ -2,6 +2,12 @@
 /*************************************************************
  * 
  * 連線至DataBase
+ * @param string $dbname 資料庫
+ * @param string $host 位址
+ * @param int $port 端口
+ * @param string $user 用戶
+ * @param string $passwd 密碼
+ * @param string $charset 字符集
  * 
 *************************************************************/
 class DBConnection{
@@ -15,28 +21,16 @@ class DBConnection{
     private $charset;
     /************************************************
      * ### DBConnection ###
-     * @param string $dbname 資料庫
-     * @param string $host 位址
-     * @param int $port 端口
-     * @param string $user 用戶
-     * @param string $passwd 密碼
-     * @param string $charset 字符集
+     
      ************************************************/
-    public function __construct($dbname, $host, $port=3306, $user, $passwd, $charset='utf8mb4'){
+    public function __construct($dbname, $host="127.0.0.1", $port=3306, $user="root", $passwd="root", $charset='utf8mb4')
+    {
         $this->host = $host;
         $this->port = $port;
         $this->user = $user;
         $this->passwd = $passwd;
         $this->charset = $charset;
         $this->resetDBname($dbname);
-    }
-    /************************************************
-     * ### 修改配置資料庫 ###
-     * @param string $dbname 資料庫
-     ************************************************/
-    public function resetDBname($dbname){
-        $this->dbname=$dbname;
-        $this->setDBConnection();
     }
     /************************************************
      * ### 設置DB資訊 ###
@@ -46,7 +40,8 @@ class DBConnection{
      * @param string $passwd 密碼
      * @param string $charset 字符集
      ************************************************/
-    private function setDBConnection(){
+    private function setDBConnection()
+    {
         $dsn="mysql:dbname=$this->dbname;host=$this->host;port=$this->port;charset=$this->charset";
         try{
             $this->connection=new PDO($dsn, $this->user,$this->passwd);
@@ -58,17 +53,27 @@ class DBConnection{
         //$link = new PDO($dsn,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_PERSISTENT => false));
     }
     /************************************************
+     * ### 修改配置資料庫 ###
+     * @param string $dbname 資料庫
+     ************************************************/
+    public function resetDBname($dbname)
+    {
+        $this->dbname=$dbname;
+        $this->setDBConnection();
+    }
+    /************************************************
      * ### 抓取單筆資料 ###
      * @param string $SQL SQL語句
      * @param bool $print_r 是否直接印出
      ************************************************/
-    public function single($SQL,$print_r=false){
+    public function single($SQL,$print_r=false) : array
+    {
         try{
             $outputValue=$this->connection->query($SQL)->fetch(PDO::FETCH_ASSOC);
             return $print_r ? print_r($outputValue,true) : $outputValue;
         }catch(PDOException $e){
             if($this->debugmode) exit($e);
-            else exit('Error: single Catch Data was failed.');
+            else exit('Error: Single catch data was failed.');
         }
     }
     /************************************************
@@ -76,13 +81,14 @@ class DBConnection{
      * @param string $SQL SQL語句
      * @param bool $print_r 是否直接印出
      ************************************************/
-    public function each($SQL,$print_r=false){
+    public function each($SQL,$print_r=false) : array
+    {
         try{ 
             $outputValue=$this->connection->query($SQL)->fetchAll(PDO::FETCH_ASSOC);
             return $print_r ? print_r($outputValue,true) : $outputValue;
         }catch(PDOException $e){
             if($this->debugmode) exit($e);
-            else exit('Error: each Catch Datas was failed.');
+            else exit('Error: each catch datas was failed.');
         }
     }
     // 
@@ -92,7 +98,8 @@ class DBConnection{
      * @param array $DATA 代填入值
      * @param bool $print_r 是否直接印出
      ************************************************/
-    public function prepare($SQL,$DATA=array(null),$print_r=false){
+    public function prepare($SQL,$DATA=array(null),$print_r=false) : array
+    {
         try{
             if(!is_array($DATA)){throw new Exception("Error: wrong data type.");}
             // $SQL = "SELECT * FROM table WHERE col1 = ?,col2 = ?, col3= ?"
@@ -103,14 +110,15 @@ class DBConnection{
             return $print_r ? print_r($outputValue,true) : $outputValue;
         }catch(PDOException $e){
             if($this->debugmode) exit($e);
-            else exit('Error: prepare Catch Datas was failed.');
+            else exit('Error: prepare catch datas was failed.');
         }
     }
     /************************************************
      * ### 設置debug模式 ###
      * @param bool $isdebug 設置debug模式
      ************************************************/
-    public function deBugMode($isdebug=true){
+    public function deBugMode($isdebug=true)
+    {
         $this->debugmode=$isdebug;
     }
 }?>
