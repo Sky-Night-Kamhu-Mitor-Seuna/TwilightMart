972f0a89171c25278016dc0c840825b264e1949e
Mai.Coding <74230665+0x0607@users.noreply.github.com>
2023-12-09 20:40:07 +0800

    更新購物車

--- javascripts/cart.js
+++ javascripts/cart.js
@@ -1,11 +1,16 @@
+/****************************************************************************************
+ * 
+ * 購物車
+ * 
+ ****************************************************************************************/
+// 更新購物車
 $(function(){
-	$('div.card[data-id]').on('click','button.btn',function(){
-		let productId = $(this).parents('.card').data('id');
-		let productName = $(this).parents('.card').find('.card-title').text();
-		
-		updateCartItem(productId, productName, 1);
-		updateCartItemView();
-	});
+	// $('div.card[data-id]').on('click','button.btn',function(){
+	// 	let productId = $(this).parents('.card').data('id');
+	// 	let productName = $(this).parents('.card').find('.card-title').text();
+	// 	updateCartItem(productId, productName, 1);
+	// 	updateCartItemView();
+	// });
 	
 	$('#CartView').on('click',function(){
 		$('#floatingShoppingList').toggleClass('d-none');
@@ -16,21 +21,40 @@ $(function(){
 	});
 });
 function updateCartItemView(){
-	$('#floatingShoppingList .card-body').empty();
+	$('#floatingShoppingList .list').empty();
 	//從資料庫查詢資料
 	$.ajax({
-	  url: 'http://127.0.0.1/',
+	  url: '/api/api.shopping_cart.php',
 	  method: 'POST',
 	  data: {
-		userId: '11123'
+		action: 'VIEW'
 	  },
 	  success:function(res){
 			// 測試用
-			let arr = (localStorage.getItem('cart'))?JSON.parse(localStorage.getItem('cart')):{};
-			
-			for(let o in arr){
-				$('#floatingShoppingList .card-body').append(
-					`<div><div class="d-inline-block">${arr[o].name}</div> <input onchange="updateCartItem('${o}', '${arr[o].name}', this.value, true);" type="number" value="${arr[o].amount}" /> <button onclick="deleteCartItem('${o}');" class="btn btn-danger">刪除</button></div>`
+			// let arr = (localStorage.getItem('cart'))?JSON.parse(localStorage.getItem('cart')):{};
+			console.log(res);
+			for(let o in res){
+				$('#floatingShoppingList .list').append(
+					`<!-- ${res[o].product_id} -->
+					<div class="productItem">
+					<div class="productImage" >
+						<a href=""><img src="/assets/images/4156_G_1598386817236.jpg" style="max-width: 100px;"></a> <!-- 第一欄 -->
+					</div>
+					<div style="width: 65%;text-align: center;">
+						<!-- #商品名稱# / #尺寸# / #顏色# -->
+						${res[o].name} <!--/ ${res[o].specification} /${res[o].color}-->
+					</div>
+					<div style="width: 20%;text-align: center;"> 
+						${res[o].price}
+					</div>
+					<div style="width: 10%;text-align: center;">
+						${res[o].quantity}
+						<!-- <form style="text-align: center;">
+						<input onchange="updateCartItem('${o}', '${res[o].name}', this.value, true);" type="number" value="${res[o].amount}" />
+						</form> -->
+					</div>
+					<button class="deleteProduct material-icons" style="width: 50px;" onclick="deleteCartItem('${o}');">&#xe872</button>
+					</div>`
 				);
 			}
 		},
@@ -40,25 +64,30 @@ function updateCartItemView(){
 	});
 }
 
-function updateCartItem(productId, productName, amount, isUpdating){
-	amount = parseInt(amount);
+function updateCartItem(productId, productQuantity, productSpecification, productColor){
+	productQuantity = parseInt(productQuantity);
 
 	// 新增資料到資料庫
 	$.ajax({
-	  url: 'http://127.0.0.1/',
+	  url: '/api/api.shopping_cart.php',
 	  method: 'POST',
 	  data: {
-		id: productId,
-		name: productName,
-		amount: amount,
+		action: 'UPDATE',
+		product_id: productId,
+		product_quantity: productQuantity,
+		product_specification: productSpecification,
+		product_color: productColor,
 	  },
 	  success:function(res){
 			// 測試用
+			let cartId = Object.keys(res)[0];
 			let arr = (localStorage.getItem('cart'))?JSON.parse(localStorage.getItem('cart')):{};
-			
-			arr[productId] = {
-				name: productName,
-				amount: (arr[productId]?.amount && !isUpdating)?arr[productId].amount+amount:amount
+			arr[cartId] = {
+				name: res[cartId]['name'],
+				spec: res[cartId]['specification'],
+				color: res[cartId]['color'],
+				// amount: (arr[productId]?.amount && !isUpdating)?arr[productId].amount+amount:amount
+				amount:  res[cartId]['quantity']
 			};
 			
 			localStorage.setItem('cart', JSON.stringify(arr));
@@ -69,22 +98,20 @@ function updateCartItem(productId, productName, amount, isUpdating){
 	});
 };
 
-function deleteCartItem(productId){
+function deleteCartItem(cartId){
 	// 新增資料到資料庫
 	$.ajax({
-	  url: 'http://127.0.0.1/',
+	  url: '/api/api.shopping_cart.php',
 	  method: 'POST',
 	  data: {
-		id: productId
+		action: 'DEL',
+		cart_id: cartId
 	  },
 	  success:function(res){
 			// 測試用
-			let arr = (localStorage.getItem('cart'))?JSON.parse(localStorage.getItem('cart')):{};
-			
-			delete arr[productId];
-			
-			localStorage.setItem('cart', JSON.stringify(arr));
-			
+			// let arr = (localStorage.getItem('cart'))?JSON.parse(localStorage.getItem('cart')):{};
+			// delete arr[cartId];
+			// localStorage.setItem('cart', JSON.stringify(arr));
 			updateCartItemView();
 		},
 	  error:function(err){
