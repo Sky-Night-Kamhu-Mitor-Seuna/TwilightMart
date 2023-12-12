d66b3e53dfa1ff4692a59a77820afc263974e3d8
Mai.Coding <74230665+0x0607@users.noreply.github.com>
2023-12-12 17:06:06 +0800

    更新權限覆蓋

--- SettingPermissions.sh
+++ SettingPermissions.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-sudo chown $USER:apache * -R
 chmod 775 ./cache
 chmod 775 ./templates_c
+chmod 775 ./assets/uploads -R
 rm ./templates_c/* -rf
 rm ./cache/* -rf
 touch ./templates_c/test.log
 touch ./cache/test.log
-sudo chown $USER:apache * -R
+chown $USER:apache * -R
 restorecon -Rv .
+sudo semanage fcontext -a -t httpd_sys_rw_content_t "./assets/uploads(/.*)?"
+sudo restorecon -RvF ./assets/uploads
