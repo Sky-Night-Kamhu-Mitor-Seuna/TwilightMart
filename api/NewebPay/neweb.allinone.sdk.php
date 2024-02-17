f67d23c4319d56b0517d74ba54a1e5e913f7a433
Mai.Coding <74230665+0x0607@users.noreply.github.com>
2024-02-18 00:57:00 +0800

    #02182024 api\NewebPay暫版

--- api/NewebPay/neweb.allinone.sdk.php
+++ api/NewebPay/neweb.allinone.sdk.php
@@ -1,110 +1,141 @@
 <?php
+// include "./class.connectDatabase.php";
+/****************************************************************************************
+ * 
+ * 藍新支付串接
+ * Version: 2.0.0.0 初版 By 5026.snkms 再版 By MaizuRoad.snkms
+ * @param obj $setting 一些配置資料
+ * 
+ ****************************************************************************************/
+class NewebPay_MPG_API
+{
+	private $debugmode = false;												// 除錯模式
+	private $version = "1.6";												// API程式版本
+	private $merchantId = "";												// MerchantID
+	private $hashKey = "";													// Hashkey
+	private $hashIV = "";													// HashIV
+	private $minimumPrice = 35;												// 最低容許交易金額
+	private $respondType = "JSON";											// 接收藍新返回格式
+	private $serviceURL = "https://ccore.newebpay.com/MPG/mpg_gateway";		// API服務位置 
+	private $returnURL = "";												// 支付完成，返回商店網址
+	private $notifyURL = "../api/newebpay/api.done.php";					// 支付通知網址
+	private $customerURL = "";												// 商店取號網址
+	private $clientBackURL = "";											// 返回商店網址
+	private $expireDate = 3;												// 付款期限
+	private $disableATM = false;											// 是否關閉ATM付款
+	// private $merchantPrefix;												// 訂單編號前導字元
+	private $merchantTradeNo;												// 訂單編號
+	private $orderTitle;													// 訂單名稱
+	private $amount;														// 訂單價格
 
-class NewebPay_MPG_API{
-	public $MerchantID;
-	public $HashKey;
-	public $HashIV;
-	public $ServiceURL;
-	public $ReturnURL;
-	public $NotifyURL;
-	public $CustomerURL;
-	public $ClientBackURL;
-	public $Order_Title;
-	public $Amount;
-	public $Version;
-	public $ExpireDate;
-	public $MerchantTradeNo;
-	public $MerchantPrefix;
-	public $DisableATM;
-	
-	public function getOutput(){
-		/* 送給藍新資料 */
-		$trade_info_arr = array(
-			'MerchantID' => $this->MerchantID, //商店代號
-			'RespondType' => 'JSON', //使用JSON格式
-			'TimeStamp' => time(), //時間戳
-			'Version' => $this->Version, //程式版本
-			'MerchantOrderNo' => $this->MerchantTradeNo, //商品編號
-			'Amt' => $this->Amount, //商品價格
-			'ItemDesc' => $this->Order_Title, //商品名稱
-			'ReturnURL' => $this->ReturnURL, //支付完成 返回商店網址
-			'NotifyURL' => $this->NotifyURL, //支付通知網址
-			'CustomerURL' =>$this->CustomerURL, //商店取號網址
-			'ClientBackURL' => $this->ClientBackURL , //支付取消 返回商店網址
-			'ExpireDate' => date("Y-m-d" , time()+86400*$this->ExpireDate), //繳費期限
-			'VACC' => 1, // 使用 ATM轉帳
-			'CVS' => 1 // 使用CVS付款
-		);
-		
-		if($this->DisableATM) $trade_info_arr['CVS'] = 0;
-		
-		$TradeInfo = $this->create_mpg_aes_encrypt($trade_info_arr, $this->HashKey, $this->HashIV);
-		$SHA256 = strtoupper(hash("sha256", $this->SHA256($this->HashKey,$TradeInfo,$this->HashIV)));
-		
-		echo $this->createForm($this->ServiceURL,$this->MerchantID,$TradeInfo,$SHA256,$this->Version);
+	public function __construct($setting)
+	{
+		$this->hashKey = $setting['hashKey'];
+		$this->hashIV = $setting['hashIV'];
+		$this->merchantId = $setting['merchantId'];
+		// $this->merchantPrefix = strtoupper($setting['merchantPrefix']);
+
+		if (isset($setting['respondType'])) $this->respondType = $setting['respondType'];
+		if (isset($setting['returnURL'])) $this->returnURL = $setting['returnURL'];
+		if (isset($setting['notifyURL'])) $this->notifyURL = $setting['notifyURL'];
+		if (isset($setting['customerURL'])) $this->customerURL = $setting['customerURL'];
+		if (isset($setting['clientBackURL'])) $this->clientBackURL = $setting['clientBackURL'];
+		if (isset($setting['expireDate'])) $this->expireDate = $setting['expireDate'];
+		if (isset($setting['disableATM'])) $this->disableATM = $setting['disableATM'];
 	}
-	
-	public function getOutputJSON(){
-		/* 送給藍新資料 */
-		$trade_info_arr = array(
-			'MerchantID' => $this->MerchantID, //商店代號
-			'RespondType' => 'JSON', //使用JSON格式
-			'TimeStamp' => time(), //時間戳
-			'Version' => $this->Version, //程式版本
-			'MerchantOrderNo' => $this->MerchantTradeNo, //商品編號
-			'Amt' => $this->Amount, //商品價格
-			'ItemDesc' => $this->Order_Title, //商品名稱
-			'ReturnURL' => $this->ReturnURL, //支付完成 返回商店網址
-			'NotifyURL' => $this->NotifyURL, //支付通知網址
-			'CustomerURL' =>$this->CustomerURL, //商店取號網址
-			'ClientBackURL' => $this->ClientBackURL , //支付取消 返回商店網址
-			'ExpireDate' => date("Y-m-d" , time()+86400*$this->ExpireDate), //繳費期限
-			'VACC' => 1, // 使用 ATM轉帳
-			'CVS' => 1 // 使用CVS付款
-		);
-		
-		if($this->DisableATM) $trade_info_arr['CVS'] = 0;
-		
-		$TradeInfo = $this->create_mpg_aes_encrypt($trade_info_arr, $this->HashKey, $this->HashIV);
-		$SHA256 = strtoupper(hash("sha256", $this->SHA256($this->HashKey,$TradeInfo,$this->HashIV)));
-		
-		
-		$result = array(
-			"name"=>"newebpay",
-			"url"=>$this->ServiceURL,
-			"id"=>$this->MerchantID,
-			"items"=>$TradeInfo,
-			"sha"=>$SHA256,
-			"version"=>$this->Version,
-		);
-		
-		echo json_encode($result);
+	/************************************************
+	 * ### 設置訂單資訊 ###
+	 * @param string $merchantTradeNo 訂單編號
+	 * @param string $orderTitle 訂單標題
+	 * @param int $amount 訂單金額
+	 ************************************************/
+	public function settingOrderInformation($merchantTradeNo, $orderTitle, $amount = 0): bool
+	{
+		// $merchantTradeNo = $this->merchantPrefix . $snowflakeId;
+		if ($amount <= $this->minimumPrice) return false;
+		// if (!preg_match('/^[A-Z]{1,8}[0-9]{18,20}$/', $merchantTradeNo)) return false;
+		$this->merchantTradeNo = $merchantTradeNo;
+		$this->orderTitle = $orderTitle;
+		$this->amount = (int)$amount/1;
+		return true;
 	}
-	
-	/*HashKey AES 加解密 */
-	function create_mpg_aes_encrypt ($parameter = null , $key = "", $iv = "") {
+	/************************************************
+	 * ### 取得傳送至藍新陣列格式 ###
+	 ************************************************/
+	private function getTradeInformationArray(): array
+	{
+		$tradeInformationArray = [
+			'MerchantID' => $this->merchantId,									// 商店代號
+			'RespondType' => $this->respondType,								// 接收藍新返回格式
+			'TimeStamp' => time(),												// 時間戳
+			'Version' => $this->version,										// API程式版本
+			'MerchantOrderNo' => $this->merchantTradeNo,						// 訂單編號
+			'Amt' => $this->amount,												// 訂單價錢
+			'ItemDesc' => $this->orderTitle,									// 訂單標題
+			'ReturnURL' => $this->returnURL,									// 支付完成，返回商店網址
+			'NotifyURL' => $this->notifyURL,									// 支付通知網址
+			'CustomerURL' => $this->customerURL,								// 商店取號網址
+			'ClientBackURL' => $this->clientBackURL,							// 支付取消 返回商店網址
+			'ExpireDate' => date("Y-m-d", time() + 86400 * $this->expireDate),	// 繳費期限
+			'VACC' => 1, 														// 使用ATM轉帳
+			'CVS' => 1 															// 使用CVS付款
+		];
+		if ($this->disableATM) $tradeInformationArray['CVS'] = 0;
+		$result["information"] = $this->create_mpg_aes_encrypt($tradeInformationArray, $this->hashKey, $this->hashIV);
+		$result["sha256"] = strtoupper(hash("sha256", $this->SHA256($this->hashKey, $result["information"], $this->hashIV)));
+		return $result;
+	}
+	/************************************************
+	 * ### 輸出 ###
+	 * @param int $method 格式 "JSON":單純輸出JSON、"FORM":輸出表格
+	 ************************************************/
+	public function getOutput($method = "JSON")
+	{
+		$tradeInformationArray = $this->getTradeInformationArray();
+		if (strtoupper($method) == "JSON") {
+			$result = json_encode(
+				[
+					"name" => "newebpay",
+					"url" => $this->serviceURL,
+					"id" => $this->merchantId,
+					"items" => $tradeInformationArray['information'],
+					"sha" => $tradeInformationArray['sha256'],
+					"version" => $this->version
+				]
+			);
+		} else if (strtoupper($method) == "FORM") $result = $this->createForm($tradeInformationArray['information'], $tradeInformationArray['sha256']);
+		else $result = "";
+		echo $result;
+	}
+	/************************************************
+	 * ### HashKey AES 加解密 ###
+	 ************************************************/
+	private function create_mpg_aes_encrypt($parameter = [], $key = "", $iv = "")
+	{
 		$return_str = '';
 		if (!empty($parameter)) {
 			//將參數經過 URL ENCODED QUERY STRING
 			$return_str = http_build_query($parameter);
 		}
-		return trim(bin2hex(openssl_encrypt($this->addpadding($return_str), 'aes-256-cbc', $key, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, $iv)));
+		return trim(bin2hex(openssl_encrypt($this->addpadding($return_str), 'aes-256-cbc', $key, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING, $iv)));
 	}
-	
-	function addpadding($string, $blocksize = 32) {
+	private function addpadding($string, $blocksize = 32)
+	{
 		$len = strlen($string);
 		$pad = $blocksize - ($len % $blocksize);
 		$string .= str_repeat(chr($pad), $pad);
 
 		return $string;
 	}
-
-	/*HashKey AES 解密 */
-	function create_aes_decrypt($parameter = "", $key = "", $iv = "") {
-		return $this->strippadding(openssl_decrypt(hex2bin($parameter),'AES-256-CBC', $key, OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING, $iv));
+	/************************************************
+	 * ### HashKey AES 解密 ###
+	 ************************************************/
+	public function create_aes_decrypt($parameter = "")
+	{
+		return $this->strippadding(openssl_decrypt(hex2bin($parameter), 'AES-256-CBC', $this->hashKey, OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING, $this->hashIV));
 	}
-
-	function strippadding($string) {
+	private function strippadding($string)
+	{
 		$slast = ord(substr($string, -1));
 		$slastc = chr($slast);
 		$pcheck = substr($string, -$slast);
@@ -115,43 +146,50 @@ class NewebPay_MPG_API{
 			return false;
 		}
 	}
-
-	/*HashIV SHA256 加密*/
-	function SHA256($key="", $tradeinfo="", $iv=""){
-		$HashIV_Key = "HashKey=".$key."&".$tradeinfo."&HashIV=".$iv;
+	/************************************************
+	 * ### HashIV SHA256 加密 ###
+	 ************************************************/
+	private function SHA256($tradeinfo = "")
+	{
+		$HashIV_Key = "HashKey=" . $this->hashKey . "&" . $tradeinfo . "&HashIV=" . $this->hashIV;
 
 		return $HashIV_Key;
 	}
-
-	/*建立表單*/
-	function createForm($ServiceURL="", $MerchantID="", $TradeInfo="", $SHA256="", $VER="") {
-		$szHtml = '<!doctype html>';
-		$szHtml .='<html>';
-		$szHtml .='<head>';
-		$szHtml .='<meta charset="utf-8">';
-		$szHtml .='</head>';
-		$szHtml .='<body>';
-		$szHtml .='<form name="newebpay" id="newebpay" method="post" action="'.$ServiceURL.'" style="display:none;">';
-		$szHtml .='<input type="text" name="MerchantID" value="'.$MerchantID.'" type="hidden">';
-		$szHtml .='<input type="text" name="TradeInfo" value="'.$TradeInfo.'"   type="hidden">';
-		$szHtml .='<input type="text" name="TradeSha" value="'.$SHA256.'" type="hidden">';
-		$szHtml .='<input type="text" name="Version"  value="'.$VER.'" type="hidden">';
-		$szHtml .='</form>';
-		$szHtml .='<script type="text/javascript">';
-		$szHtml .='document.getElementById("newebpay").submit();';
-		$szHtml .='</script>';
-		$szHtml .='</body>';
-		$szHtml .='</html>';
-
-		return $szHtml;
+	/************************************************
+	 * ### 建立表單 ###
+	 * @param string $tradeInformation 交易資訊
+	 * @param string $sha256 交易雜湊 
+	 ************************************************/
+	private function createForm($tradeInformation = "", $sha256 = "", $Output2newebpay=true): string
+	{
+		$result  = "<!doctype html>";
+		$result .= "<html>";
+		$result .= "<head>";
+		$result .= "<meta charset='utf-8'>";
+		$result .= "</head>";
+		$result .= "<body>";
+		$result .= "<form name='newebpay' id='newebpay' method='post' action='{$this->serviceURL}' style=display:none;>";
+		$result .= "<input type='text' name='MerchantID' value='{$this->merchantId}'>";
+		$result .= "<input type='text' name='TradeInfo' value='{$tradeInformation}'>";
+		$result .= "<input type='text' name='TradeSha' value='{$sha256}'>";
+		$result .= "<input type='text' name='Version'  value='{$this->version}'>";
+		$result .= "</form>";
+		if($Output2newebpay){
+			$result .= "<script type='text/javascript'>";
+			$result .= "document.getElementById('newebpay').submit();";
+			$result .= "</script>";
+		}
+		$result .= "</body>";
+		$result .= "</html>";
+		return $result;
 	}
-	
-	/*取得訂單編號*/
-	function getOrderNo(){
-		date_default_timezone_set('Asia/Taipei'); // CDT
-		$info = time();
-		$ordre_no = $this->MerchantPrefix.time().mt_rand(111,999);
-
-		return $ordre_no;
+	/************************************************
+	 * ### 設置debug模式 ###
+	 * @param bool $isdebug 設置debug模式
+	 ************************************************/
+	public function deBugMode($isdebug = true)
+	{
+		$this->debugmode = $isdebug;
+		$this->serviceURL = $this->debugmode ? "https://ccore.newebpay.com/MPG/mpg_gateway" : "https://core.newebpay.com/MPG/mpg_gateway";
 	}
 }
--- api/NewebPay/neweb.installation.php
+++ api/NewebPay/neweb.installation.php
@@ -1,23 +1,22 @@
 <?php
-	include('./neweb.allinone.sdk.php');
-	$obj = new NewebPay_MPG_API();
-	
-	//服務參數
-	$obj->ServiceURL			= "https://ccore.newebpay.com/MPG/mpg_gateway";		// API服務位置 (此為測試環境網址)
-	$obj->ReturnURL				= "";												// 支付完成，返回商店網址
-	$obj->NotifyURL				= "";												// 支付通知網址
-	$obj->CustomerURL			= "";												// 商店取號網址
-	$obj->ClientBackURL			= "";												// 返回商店網址
-	
-	$obj->HashKey				= '';												// Hashkey
-	$obj->HashIV				= '';												// HashIV
-	$obj->MerchantID			= '';												// MerchantID
-	$obj->MerchantPrefix		= '';												// 訂單編號前導字元
-	$obj->MerchantTradeNo		= $this->getOrderNo();								// 訂單編號
-	$obj->Version				= '1.5';											// API程式版本
-	
-	$obj->Amount				= 0;												// 商品價格
-	$obj->Order_Title			= "";												// 商品名稱
-	$obj->ExpireDate			= 3;												// 付款期限
-	
-	$obj->getOutput();
+/****************************************************************************************
+ * 
+ * 藍新金流初始化
+ * 
+ ****************************************************************************************/
+require_once "../sys.global.php";
+/****************************************************************************************/
+// 藍新支付相關資訊
+require_once('neweb.allinone.sdk.php');
+$newPaySetting = [
+    "hashKey" => STORE_HASH_KEY,
+    "hashIV" => STORE_HASH_IV,
+    "merchantId" => STORE_ID,
+    // "merchantPrefix" => STORE_PREFIX,
+    "returnURL" => STORE_RETURN_URL,
+    "notifyURL" => STORE_NOTIFY_URL,
+    "clientBackURL" => STORE_CLIENT_BACK_URL,
+    "respondType" => "String"
+];
+$newebPayObj = new NewebPay_MPG_API($newPaySetting);
+$newebPayObj->deBugMode(CONFIG_GENERAL['debug']);
